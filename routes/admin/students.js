d3d1bb67d6150a564c88dc00f6f8aafadf0fcb91
azizkhoso <azizkhoso586@gmail.com>
2023-06-09 22:34:10 +0500

    feat: added delete and get tests routes for student router in admin

--- routes/admin/students.js
+++ routes/admin/students.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Student = require('../../models/student');
+const Submission = require('../../models/submission');
 
 const adminStudentsRouter = express.Router();
 
@@ -14,5 +15,26 @@ adminStudentsRouter.get('/', async (req, res) => {
     res.status(500).json({ error: e.message });
   }
 });
+
+// DELETE /admin/students/:id
+adminStudentsRouter.delete('/:id', async (req, res) => {
+  try {
+    const student = await Student.findByIdAndDelete(req.params.id);
+    if (!student) throw new Error('Student not found');
+    res.json(student);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+adminStudentsRouter.get('/:id/tests', async (req, res) => {
+  try {
+    const submission = await Submission.findOne({ submittedBy: req.params.id }).populate('test');
+    if (!submission) throw new Error('Test Submission not found');
+    res.json({ submission });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+})
 
 module.exports = adminStudentsRouter;
