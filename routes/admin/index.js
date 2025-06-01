21e173687fe18a5dbbd71bd78d371a0fbc29da08
azizkhoso <azizkhoso586@gmail.com>
2025-06-01 11:28:18 +0500

    fix: redo changes of submissions

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -245,34 +245,14 @@ router.get('/submissions/:testId', async (req, res) => {
     // add pagination
     const lim = 10;
     const page = Number(req.query.page || 0);
-    // if admin get all submissions instead of filtering by teacher
-    if (!req.teacher) {
-      const totalSubmissions = await Submission.count();
-      const submissions = await Submission.find()
-        .populate({ path: 'test' })
-        .populate('submittedBy')
-        .sort({ totalCorrect: 'desc' })
-        .skip(page * lim)
-        .limit(lim);
-      res.json({ submissions, total: totalSubmissions, page, limit: lim });
-    } else {
-      // else do the following
-      // get submissions of those tests that were created by req.teacher._id
-      const testsQuery = Test.find().select('_id');
-      if (req.teacher?._id) {
-        testsQuery.where({ createdBy: req.teacher._id });
-      }
-      const tests = await testsQuery.exec();
-      const ids = tests.map((t) => t._id);
-      const totalSubmissions = await Submission.count({ test: { $in: ids } });
-      const submissions = await Submission.find({ test: { $in: ids } })
-        .populate({ path: 'test' })
-        .populate('submittedBy')
-        .sort({ totalCorrect: 'desc' })
-        .skip(page * lim)
-        .limit(lim);
-      res.json({ submissions, total: totalSubmissions, page, limit: lim });
-    }
+    const totalSubmissions = await Submission.count({ test: req.params.testId });
+    const submissions = await Submission.find({ test: req.params.testId })
+      .populate({ path: 'test' })
+      .populate('submittedBy')
+      .sort({ totalCorrect: 'desc' })
+      .skip(page * lim)
+      .limit(lim);
+    res.json({ submissions, total: totalSubmissions, page, limit: lim });
   } catch (e) {
     console.error(e);
     res.status(500).json({ error: e.message });
